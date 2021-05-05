6f66d1912a45e0e843d088555705b65bb1135d61
Scott Bailey <csb5t@virginia.edu>
2021-05-05 15:16:29 -0400

    Add home page

---
+++ ui_pages/home.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+
+def home():
+    description = """This application is a front-end for proof-of-concept explorations
+    in connecting the NC State University Libraries' catalog and website. Each page
+    indicates a different method or approach to analysis.
+    """
+
+    st.write(description)
